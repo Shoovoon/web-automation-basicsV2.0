8788f2dcc22a1c18610e2f4c31a11f8373ea81cc
Shahriar Allam <42257068+Shoovoon@users.noreply.github.com>
2024-02-11 18:32:58 +0600

    Learning different types of locator

--- src/test/java/webautomationv1/locationExample.java
+++ src/test/java/webautomationv1/locationExample.java
@@ -6,8 +6,10 @@ import org.testng.annotations.Test;
 
 public class locationExample extends BaseDriver {
 
-	String url = "http://aims.baf.org";
-
+//	String url = "http://aims.baf.org";
+	String url = "https://demo.nopcommerce.com/";
+	
+	
 	@Test
 	public void locationTest() throws InterruptedException {
 		
@@ -16,15 +18,47 @@ public class locationExample extends BaseDriver {
 		
 //		to store : webelement
 		
+//Practice Site: AIMS:
+		
+		
+////		locate by ID
+//		WebElement priMenu = driver.findElement(By.id("service_number"));
+//		priMenu.click();  //clicks on link with desired ID
+//		priMenu.sendKeys("Support17104");  //to sent value
+//		
+//		driver.findElement(By.xpath("//button[contains(text(),'Log in')]")).click();
+////		driver.findElement(By.xpath("//b[contains(text(),'Forgot Password')]")).click();
+//		Thread.sleep(10000); 
+		
+		
+		
+//Practice Site: NopCommerce
 		
 //		locate by ID
-		WebElement priMenu = driver.findElement(By.id("service_number"));
-		priMenu.click();
-		priMenu.sendKeys("Support17104");
 		
-		driver.findElement(By.xpath("//button[contains(text(),'Log in')]")).click();
-//		driver.findElement(By.xpath("//b[contains(text(),'Forgot Password')]")).click();
-		Thread.sleep(10000); 
+		driver.findElement(By.xpath("//a[contains(text(),'Log in')]")).click();
+	
+//	bY nAME
+		
+		WebElement emailID = driver.findElement(By.name("Email"));
+		emailID.sendKeys("email@email.com");
+		Thread.sleep(5000);
+		
+		
+// by LinkText:
+		
+		WebElement forgetPass = driver.findElement(By.linkText("Forgot password?"));
+		forgetPass.click();
+		Thread.sleep(5000);
+
+		
+//		by PartialLinkText
+		
+		WebElement partialLinkText = driver.findElement(By.partialLinkText("Gift"));
+		partialLinkText.click();
+		Thread.sleep(10000);
+		
+		
 		
 	}
 
